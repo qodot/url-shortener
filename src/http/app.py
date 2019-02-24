5603f39038a30409079649e4107889aa3084451b
qodot <waitingforqodot@gmail.com>
2019-02-24 13:17:15 +0900

    단축 URL 생성 템플릿, API 추가

--- src/http/app.py
+++ src/http/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
+
+from src.service.url import UrlShortenerService
+from src.domain.url import OriginUrl, ShortenHash
+from src.infra.url_repository import SAUrlRepository
 
 
 def create_app():
@@ -13,3 +17,15 @@ app = create_app()
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    origin_url = request.form.get('origin')
+    if not origin_url:
+        abort(400, 'origin URL is required')
+
+    service = UrlShortenerService(SAUrlRepository())
+    shorten_hash: ShortenHash = service.shortify(OriginUrl(origin_url))
+
+    return render_template('generated.html', shorten=shorten_hash.hash)
