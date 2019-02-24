e70125dbc9a896d975fd05a9ecaba060606c02f3
qodot <waitingforqodot@gmail.com>
2019-02-24 15:22:16 +0900

    데이터베이스 URI를 환경변수에서 받아오게 수정

--- src/infra/sqlalchemy.py
+++ src/infra/sqlalchemy.py
@@ -1,12 +1,14 @@
+import os
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
+PG_URI = os.environ.get('URL_SHORTENER_PG_URI')
 uri = f'postgresql://postgres:1234@localhost:5432/url-shortener'
 
-engine = create_engine(uri, echo=True)
+engine = create_engine(PG_URI, echo=True)
 session_factory = sessionmaker(bind=engine, expire_on_commit=False)
 Session = scoped_session(session_factory)
 
@@ -21,6 +23,7 @@ def tx():
         yield session
     except Exception:
         session.rollback()
+        raise
     else:
         session.commit()
     finally:
