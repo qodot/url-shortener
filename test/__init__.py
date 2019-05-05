d773ff768fe05c4f0c631ee7f1228f7036710ce3
qodot <waitingforqodot@gmail.com>
2019-05-05 17:51:09 +0900

    DB 테스트를 위한 TestDB 클래스 작성
    - 프로덕션 코드에서 데이터베이스 commit이 일어나지 않음

--- test/__init__.py
+++ test/__init__.py
@@ -9,3 +9,20 @@ def pytest_not_raises(exception_class):
         yield
     except exception_class:
         raise pytest.fail('DID RAISE {}'.format(exception_class))
+
+
+def dont_commit(session):
+    @contextmanager
+    def fake_begin():
+        yield session
+
+    return fake_begin
+
+
+class TestDB:
+    @pytest.fixture(scope='function', autouse=True)
+    def session_lifecycle(self, mocker, sa_session_factory, sa_session):
+        mocker.patch(
+            'src.infra.sqlalchemy.Session.begin',
+            new=dont_commit(sa_session),
+        )
